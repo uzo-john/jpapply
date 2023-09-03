257b3d3a9205d04eff88a4c3e80568d64dde4375
uzo-john <johnpauluzowuru2018@gmail.com>
2023-09-03 07:41:37 -1200

    my first deploy

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
       </div>
       
         <ul class="social-links">
-          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="@JohnpaulUzowuru"><i class="fab fa-facebook"></i></a></li>
           <li><a href="#"><i class="fab fa-twitter"></i></a></li>
           <li><a href="#"><i class="fab fa-instagram"></i></a></li>
         </ul>
